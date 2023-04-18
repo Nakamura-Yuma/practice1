541877f424486d2b31eea327f27961575e1752d9
UMA <c0b22112db@edu.teu.ac.jp>
2023-04-18 17:52:14 +0900

    演習課題2

--- pygame_image.py
+++ pygame_image.py
@@ -16,9 +16,10 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        x = tmr % 1600
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(pg.transform.flip(bg_img, True, False), [1600-x, 0]) 
+        screen.blit(bg_img, [3200-x, 0])
         screen.blit(kk_imgs[(tmr // 50) % 2], [300,200])
 
         pg.display.update()
